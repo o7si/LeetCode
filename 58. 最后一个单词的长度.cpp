ac11d9d2917342414da65fd79cf06c0e606e0ae6
o7si <o7si@foxmail.com>
2019-09-16 13:54:39 +0800

    feat: problem 52, 58

---
+++ 58. 最后一个单词的长度.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        
+        int i = s.size() - 1;
+        while (i >= 0 && s[i] == ' ')
+            -- i;
+
+        if (i < 0)
+            return 0;
+
+        int j = i;
+        while (j >= 0 && s[j] != ' ')
+            -- j;
+
+        return i - j;
+    }
+};
